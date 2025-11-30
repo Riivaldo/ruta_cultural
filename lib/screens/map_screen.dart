a397f53b7405ec680e028c14ef86635451d991bf
1Mi2Nombre3 <jhoonn71296996@gmail.com>
2025-11-29 23:58:24 -0400

    Mejora de diseño + idioma

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/map_screen.dart
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
--- lib/services/favoritos.dart
+++ lib/services/favoritos.dart
@@ -1,4 +1,3 @@
-// lib/services/favoritos.dart
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
